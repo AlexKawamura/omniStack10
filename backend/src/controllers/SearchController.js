7a00a3e9dba91a7d951d6c54c33e8e587b057cc0
ALEXANDRE KAWAMURA TANAKA <alexandre.k.tanaka@gmail.com>
2020-06-24 21:31:09 -0300

    Criando query para filtragem na busca por devs

--- backend/src/controllers/SearchController.js
+++ backend/src/controllers/SearchController.js
@@ -7,10 +7,22 @@ module.exports = {
 
         const techsArray = parseStringAsArray(techs);
 
+        // Query para procura por devs com Filtros
         const devs = await Dev.find({
-
+            techs: {
+                $in: techsArray, // operador lógico do mongodb para filtrar por valores iguais
+            },
+            location: {
+                $near: { // operador para filtrar pelos valores mais próximos
+                    $geometry: { // operador para especificar as coordenadas
+                        type: 'Point',
+                        coordinates: [longitude, latitude],
+                    },
+                    $maxDistance: 10000, // define uma distância máxima para filtrar
+                },
+            },
         });
 
-        return response.json({ devs: [] });
+        return response.json({ devs });
     }
 }
