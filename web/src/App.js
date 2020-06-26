b0424e58503ec2683b00fa8538330381cefd55b1
ALEXANDRE KAWAMURA TANAKA <alexandre.k.tanaka@gmail.com>
2020-06-25 22:46:31 -0300

    Criando o formulario

--- web/src/App.js
+++ web/src/App.js
@@ -7,7 +7,32 @@ function App() {
   return (
     <div id="app">
       <aside>
+        <strong>Cadastrar</strong>
+        <form>
+          <div class="input-block">
+            <label htmlFor="github_username">Usuario do Github</label>
+            <input name="github_username" id="github_username" required></input>
+          </div>
 
+          <div class="input-block">
+            <label htmlFor="techs">Tecnologias</label>
+            <input name="techs" id="techs" required></input>
+          </div>
+
+          <div className="input-group">
+            <div class="input-block">
+              <label htmlFor="latitude">Latitude</label>
+              <input name="latitude" id="latitude" required></input>
+            </div>
+
+            <div class="input-block">
+              <label htmlFor="longitude">Longitude</label>
+              <input name="longitude" id="longitude" required></input>
+            </div>
+          </div>
+
+          <button type="submit">Salvar</button>
+        </form>
       </aside>
       <main>
 
