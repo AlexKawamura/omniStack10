080f1032126b35b29cf707967294da68ffa9453c
ALEXANDRE KAWAMURA TANAKA <alexandre.k.tanaka@gmail.com>
2020-07-13 11:34:56 -0300

    Criando rotas e estilizando o header

--- mobile/App.js
+++ mobile/App.js
@@ -1,27 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { StatusBar } from 'react-native';
+
+import Routes from './src/routes';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Hello OmniStack!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <StatusBar barStyle="light-content" backgroundColor="#7D40E7" />
+      <Routes />
+    </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#7159c1',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-
-  title: {
-    fontWeight: "bold",
-    fontSize: 32,
-    color: '#fff'
-  },
-});
